e7157ec7e4522aba03e63abe3f48e975c7955767
sebahsr <sebahsr@gmail.com>
2024-07-23 12:31:07 +0000

    information security lessons

--- myApp.js
+++ myApp.js
@@ -1,7 +1,26 @@
 const express = require('express');
+const helmet = require('helmet')
 const app = express();
-
-
+let timeInSeconds= 90*24*60*60;
+app.use(helmet.hidePoweredBy());
+app.use(helmet.xssFilter())
+app.use(helmet.noSniff());
+app.use (helmet.ieNoOpen());
+app.use(helmet.hsts({maxAge: timeInSeconds, force: true}))
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache())
+app.use(helmet({
+  frameguard: {         // configure
+    action: 'deny'
+  },
+  contentSecurityPolicy: {    // enable and configure
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ['style.com'],
+    }
+  },
+  dnsPrefetchControl: false     // disable
+}))
 
 
 
